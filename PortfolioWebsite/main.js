ac0248f77e0304bc0752ad68ec9dd355646d7953
arkam29 <115999800+Arkam102@users.noreply.github.com>
2024-08-18 16:40:48 +0530

    Update main.js

--- PortfolioWebsite/main.js
+++ PortfolioWebsite/main.js
@@ -50,19 +50,63 @@ if (typed) {
     backDelay: 2000
   });
 }
+let isScrolling = false; // Track whether the page is currently scrolling
 
 menuLinks.forEach(link => {
   link.addEventListener('click', function(event) {
     event.preventDefault();
+
+    // Remove the 'active_link' class from all links
+    menuLinks.forEach(item => {
+      item.classList.remove('active_link');
+    });
+
+    // Add the 'active_link' class to the clicked link
+    this.classList.add('active_link');
+
     const targetId = this.getAttribute('href').substring(1);
     const targetSection = document.getElementById(targetId);
+
+    // Temporarily disable scroll event listener
+    isScrolling = true;
 
     window.scrollTo({
       top: targetSection.offsetTop - header.offsetHeight,
       behavior: 'smooth'
     });
+
+    // Re-enable scroll event listener after scrolling is done
+    setTimeout(() => {
+      isScrolling = false;
+    }, 1000); // Adjust the delay if necessary
   });
 });
+
+function handleScroll() {
+  if (isScrolling) return; // Skip updating active link during scroll from click
+
+  const header = document.getElementById('header');
+  const sections = document.querySelectorAll('section');
+  const currentScroll = window.scrollY + header.offsetHeight; 
+  header.classList.toggle('active', window.scrollY >= 20);
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    const sectionId = section.getAttribute('id');
+    const correspondingMenuItem = document.querySelector(`a[href="#${sectionId}"]`);
+
+    if (currentScroll >= sectionTop && currentScroll < sectionTop + sectionHeight) {
+      document.querySelectorAll('.menu_link').forEach((item) => {
+        item.classList.remove('active_link');
+      });
+      correspondingMenuItem.classList.add('active_link');
+    }
+  });
+}
+
+window.addEventListener('scroll', handleScroll);
+
 
 function sendMail(event) {
   event.preventDefault();
